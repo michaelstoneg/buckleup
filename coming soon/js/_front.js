ab8b4990d446fbe2302eeda9ebd35cb1f3687add
michael grandison <michael.grandison@gmail.com>
2017-03-28 10:40:43 +0100

    added subscribe form popup on landing

--- coming soon/js/_front.js
+++ coming soon/js/_front.js
@@ -128,14 +128,13 @@ jQuery(document).ready(function(){
 		});
 	}
 
-  $.ajax({
-    type: 'GET',
-    url: 'api.tumblr.com/v2/blog/prettypeachy.tumblr.com/posts?api_key=7hqHnCrISSOa2sL67JTC6YpgArXlkgun9Y9JmL9FP962MAN1d2'
-    // cache: false,
-    // success: function(result) {
-    //   console.log('response', result);
-    // }
-  });
+  // $('.subscribePopupButton').click(function () {
+  $('.subscribePopup').show();
+  // });
+  $('#subscribePopupClose').click(function () {
+    $('.subscribePopup').hide();
+  })
+
 });
 
 /* Vertical Alignment */
