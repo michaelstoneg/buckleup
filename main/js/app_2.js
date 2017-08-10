bd68d5eaa8366dc1bf044007cb56e4ffb7a88018
Michael Grandison <michael.grandison@airdates.co>
2017-08-10 18:01:24 +0100

    added seatbelt animation to job page

--- main/js/app_2.js
+++ main/js/app_2.js
@@ -20,8 +20,27 @@ $(document).ready(function(){
     $('.press_subscribe_popup_wrapper').hide();
   });
 
-if ($(window).width() < 450) {
-  console.log('mobile mode');
-}
+  if ($(window).width() < 450) {
+    console.log('mobile mode');
+  }
+
+
+  // $('.seatbelt_animation_left').click(function () {
+
+    console.log('belts up');
+    $('.seatbelt_animation_left').css({
+      animation: 'left_seatbelt 1.5s infinite'
+    });
+
+    $('.seatbelt_animation_right').css({
+      animation: 'right_seatbelt 1.5s infinite'
+    });
+
+    $('.seatbelt_animation_arrow').css({
+      animation: 'arrow_seatbelt 1.5s infinite'
+    })
+
+  // })
+
 
 });
