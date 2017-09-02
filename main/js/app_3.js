58d890d88b08e26efe02b0359b3cadf633b0880b
Michael Grandison <michael.grandison@airdates.co>
2017-09-02 13:51:51 +0100

    tech page styling icons

--- main/js/app_3.js
+++ main/js/app_3.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
 
-  $('.tech_section_center').css({visibility: "hidden"});
-  $('.tech_section_right').css({visibility: "hidden"});
+  // $('.tech_section_center').css({visibility: "hidden"});
+  // $('.tech_section_right').css({visibility: "hidden"});
 
   console.log("It's the tech page");
 
   $('.tech_section_left').mouseover(function() {
     console.log('open the gates');
-    $('.tech_section_center').css({visibility: "visible"});
-    $('.tech_section_right').css({visibility: "visible"});
+    // $('.tech_section_center').css({visibility: "visible"});
+    // $('.tech_section_right').css({visibility: "visible"});
+    $('.tech_section_center').css({
+      transformOrigin: 'top',
+      transform:scale(2,2)});
   });
 
 });
