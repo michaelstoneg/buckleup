2ef7bb6bb7060d104ca638c93cc3b76abee62673
Michael Grandison <michael.grandison@airdates.co>
2017-09-04 14:25:27 +0100

    tech page slack invite test ok

--- main/js/app_3.js
+++ main/js/app_3.js
@@ -1,17 +1,26 @@
 $(document).ready(function(){
 
-  // $('.tech_section_center').css({visibility: "hidden"});
-  // $('.tech_section_right').css({visibility: "hidden"});
+
 
   console.log("It's the tech page");
 
-  $('.tech_section_left').mouseover(function() {
-    console.log('open the gates');
-    // $('.tech_section_center').css({visibility: "visible"});
-    // $('.tech_section_right').css({visibility: "visible"});
-    $('.tech_section_center').css({
-      transformOrigin: 'top',
-      transform:scale(2,2)});
-  });
+  var addSlackMember = function(){
+    var email           = $('.tech_section_slack_input').val(),
+        // time                = new Date().getTime(),
+        // slack_team_id          = 'C5V51HJ8N',
+        joinChannels    = 'C5V51HJ8N',
+        authToken           = 'xoxp-197800860529-199171589414-236745932726-26f065e94be6d66fa8bf2a93be64cf71',
+        // inviteUrl = 'https://' + slack_team + '.slack.com/api/users.admin.invite?t=' + time;
+        inviteUrl = 'https://slack.com/api/users.admin.invite?token=' + authToken + '&email=' + email + '&channels=' + joinChannels;
+
+        $.post(inviteUrl, function() {
+          console.log(inviteUrl);
+        });
+      }
+
+    $('.tech_section_slack_button').click(function () {
+      console.log('go go gadget slack invite');
+      addSlackMember();
+    })
 
 });
