1210e76bb370911b120a33137b3ab81a8a0e25d7
Michael Grandison <michael.grandison@airdates.co>
2017-09-04 18:24:36 +0100

    tech page slack invite team change

--- main/js/app_3.js
+++ main/js/app_3.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
 
-
-
   console.log("It's the tech page");
 
   var addSlackMember = function(){
     var email           = $('.tech_section_slack_input').val(),
         // time                = new Date().getTime(),
-        // slack_team_id          = 'C5V51HJ8N',
-        joinChannels    = 'C5V51HJ8N',
-        authToken           = 'xoxp-197800860529-199171589414-236745932726-26f065e94be6d66fa8bf2a93be64cf71',
+        // joinChannels    = 'C5V51HJ8N', bot dojo test channel
+        joinChannels    = 'C6Y3KSXM3',
+        authToken           = 'xoxp-235333652672-236042182180-235456487713-9fe8fb17898293c864ebf067450fedf2',
+        // authToken           = 'xoxp-197800860529-199171589414-236745932726-26f065e94be6d66fa8bf2a93be64cf71', bot dojo test token
         // inviteUrl = 'https://' + slack_team + '.slack.com/api/users.admin.invite?t=' + time;
         inviteUrl = 'https://slack.com/api/users.admin.invite?token=' + authToken + '&email=' + email + '&channels=' + joinChannels;
 
