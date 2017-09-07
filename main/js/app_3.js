7e6555f63ebba088c9bcff9de2c62eb3472de8b9
Michael Grandison <michael.grandison@airdates.co>
2017-09-07 12:35:41 +0100

    menu and footer link fixes for all pages

--- main/js/app_3.js
+++ main/js/app_3.js
@@ -40,4 +40,22 @@ $(document).ready(function(){
       }, 800);
     });
 
+    $('.subscribe_popup_wrapper').hide();
+    $('#subscribe_general').click(function () {
+      $('.subscribe_popup_wrapper').show();
+    });
+
+    $('#subscribe_popup_close').click(function () {
+      $('.subscribe_popup_wrapper').hide();
+    });
+
+    $('.press_subscribe_popup_wrapper').hide();
+    $('#subscribe_press').click(function () {
+      $('.press_subscribe_popup_wrapper').show();
+    });
+
+    $('#press_subscribe_popup_close').click(function () {
+      $('.press_subscribe_popup_wrapper').hide();
+    });
+
 });
